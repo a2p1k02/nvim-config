eff9981cde236ae069e8d2603e960b97148f3dd3
a2p1k02 <35633190+a2p1k02@users.noreply.github.com>
2021-12-07 12:41:55 +0000

    Fix tab width

--- init.lua
+++ init.lua
@@ -10,6 +10,9 @@ vim.o.backup=false
 vim.o.termguicolors=true
 vim.o.tabstop=4
 vim.o.shiftwidth=4
+vim.o.smarttab=true
+vim.o.expandtab=true
+vim.o.softtabstop=4
 vim.o.completeopt = 'menuone,noselect'
 
 require('onenord').setup()
